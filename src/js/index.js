ad7dfb75cd7ff38faf8e4fc514225322132712fd
Kelley Muro <kelleymuro@gmail.com>
2018-12-19 23:48:36 -0700

    Built the search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-// Global app controller
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const key = '3f070dc8c31f57e4ec74a471b1452f6f';
-  try {
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,17 @@
-export default 'I am an exported string!';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    const key = '3f070dc8c31f57e4ec74a471b1452f6f';
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
