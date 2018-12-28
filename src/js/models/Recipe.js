ef31f5d95ccf67546867113373b376e7deb9b8ae
Kelley Muro <kelleymuro@gmail.com>
2018-12-28 12:27:53 -0700

    You can now increase servings size. Wish I had friends to do that for.

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -94,8 +94,15 @@ export default class Recipe {
 
   updateServings(type) {
     // Servings
+    const newServings = type === 'dec' ? this.servings -1 : this.servings + 1;
 
     // Ingredients
+    this.ingredients.forEach(ing => {
+      ing.count *= (newServings / this.servings);
+    });
+
+    this.servings = newServings;
+
   }
 
 }
